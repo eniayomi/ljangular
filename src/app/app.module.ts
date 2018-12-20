fc8551d274df428f4d08652e80b46d8e591bfc56
Pelumi <oluwaseyieniayomi@gmail.com>
2018-12-20 15:48:26 +0100

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,33 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DataService } from './data.service';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { ProfileComponent } from './profile/profile.component';
+
+import { DragulaModule } from 'ng2-dragula';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserComponent,
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
+  	HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    DragulaModule.forRoot()
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
